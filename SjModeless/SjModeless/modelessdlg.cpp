538e3e5c5ccd8cad0ff9f44a54138c4751065cd7
Hwangtaehun <r283@naver.com>
2022-08-05 22:35:09 +0900

    Modeless 대화상자와 기본 Class 간 상호 참조

---
+++ SjModeless/SjModeless/modelessdlg.cpp
@@ -0,0 +1,60 @@
+﻿// modelessdlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "SjModeless.h"
+#include "modelessdlg.h"
+#include "afxdialogex.h"
+#include "MainFrm.h"
+
+
+// CModelessDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CModelessDlg, CDialogEx)
+
+CModelessDlg::CModelessDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MODELESSDLG, pParent)
+{
+
+}
+
+CModelessDlg::~CModelessDlg()
+{
+}
+
+void CModelessDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CModelessDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BT_EXITMODELESS, &CModelessDlg::OnBnClickedExitmodeless)
+END_MESSAGE_MAP()
+
+
+// CModelessDlg 메시지 처리기
+
+
+void CModelessDlg::OnBnClickedExitmodeless()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	DestroyWindow();
+}
+
+
+void CModelessDlg::PostNcDestroy()
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	((CMainFrame*)AfxGetMainWnd())->m_pDlg = NULL;
+	delete this;
+	CDialogEx::PostNcDestroy();
+}
+
+
+BOOL CModelessDlg::Create(CWnd* pParentWnd)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+
+	return CDialogEx::Create(IDD, pParentWnd);
+}
---
+++ SjModeless/SjModeless/resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// SjModeless.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_SjModelessTYPE              130
+#define IDD_MODALDLG                    310
+#define IDD_MODELESSDLG                 312
+#define IDC_BT_EXITDLG                  1000
+#define IDC_BT_EXITMODELESS             1001
+#define ID_MODAL32771                   32771
+#define ID_MODAL32772                   32772
+#define IDM_MODAL                       32773
+#define IDM_MODELESS                    32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define IDM_APP                         32779
+#define IDM_FRM                         32780
+#define IDM_DOC                         32781
+#define IDM_VIEW                        32782
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
