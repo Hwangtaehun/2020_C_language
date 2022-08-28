0b7d51ba7d8aa4147b629113e357b7c3c6148300
Hwangtaehun <r283@naver.com>
2022-08-28 20:43:46 +0900

    지뢰찾기 기본틀

---
+++ SejongMine/CustomDlg.cpp
@@ -0,0 +1,43 @@
+﻿// CustomDlg.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+//#include "pch.h"
+#include "SejongMine.h"
+#include "CustomDlg.h"
+#include "afxdialogex.h"
+
+
+// CCustomDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CCustomDlg, CDialogEx)
+
+CCustomDlg::CCustomDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, m_nHeight(0)
+	, m_nWidth(0)
+	, m_nMine(0)
+{
+
+}
+
+CCustomDlg::~CCustomDlg()
+{
+}
+
+void CCustomDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_HEIGHT, m_nHeight);
+	DDV_MinMaxInt(pDX, m_nHeight, 1, 24);
+	DDX_Text(pDX, IDC_EDIT_WIDTH, m_nWidth);
+	DDV_MinMaxInt(pDX, m_nWidth, 1, 30);
+	DDX_Text(pDX, IDC_EDIT_MINE, m_nMine);
+}
+
+
+BEGIN_MESSAGE_MAP(CCustomDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCustomDlg 메시지 처리기
---
+++ SejongMine/CustomDlg.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+
+// CCustomDlg 대화 상자
+
+class CCustomDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CCustomDlg)
+
+public:
+	CCustomDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CCustomDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_nHeight;
+	int m_nWidth;
+	int m_nMine;
+};
